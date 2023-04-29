5ed7c5f466d697b83a50fcbddc6e3e2011305958
Mauricio Castelazo <0236968@up.edu.mx>
2023-04-29 10:08:53 -0600

    Initial Commit

---
+++ App_H/App_HApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_HApp.swift
+//  App_H
+//
+//  Created by Mauricio Castelazo on 29/04/23.
+//
+
+import SwiftUI
+
+@main
+struct App_HApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
