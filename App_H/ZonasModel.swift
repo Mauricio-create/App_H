777a7b459c14a1fce4bb4e95ddb37cdb2fad2339
Mauricio Castelazo <0236968@up.edu.mx>
2023-04-30 08:19:33 -0600

    zone

--- App_H/ZonasModel.swift
+++ App_H/ZonasModel.swift
@@ -15,12 +15,13 @@ struct ZonasModel: Identifiable,Codable, Hashable{
     var latitud:Double
     var longitud:Double
     var costo:Double
-    
+    var cap_max:Int
+    var disp:Int
 }
 extension ZonasModel{
     static let Zonas=[
-        ZonasModel(id:UUID(),nombre:"Plaza Universidad",latitud:19.367404116160845, longitud: -99.16685200327753, costo: 50),
-        ZonasModel(id:UUID(),nombre:"Lugar B",latitud:19.367404116160845, longitud: -90.16685200327753, costo:0),
-        ZonasModel(id:UUID(),nombre:"Lugar C",latitud:19.367404116160845, longitud: -93.16685200327753,, costo:0),
+        ZonasModel(id:UUID().uuidString,nombre:"Plaza Universidad",latitud:19.367404116160845, longitud: -99.16685200327753, costo: 50, cap_max:100, disp:100),
+        ZonasModel(id:UUID().uuidString,nombre:"Lugar B",latitud:19.367404116160845, longitud: -90.16685200327753, costo:0, cap_max:200, disp:200),
+        ZonasModel(id:UUID().uuidString,nombre:"Lugar C",latitud:19.367404116160845, longitud: -93.16685200327753, costo:0, cap_max:300, disp:300)
     ]
 }
