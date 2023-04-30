d153b8349df83fa68512e015f37c49c4cce7ace2
AdrianReyes19 <0231620@up.edu.mx>
2023-04-29 18:19:10 -0600

    Base de Datos estacionamiento

--- App_H/App_HApp.swift
+++ App_H/App_HApp.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 import FirebaseCore
-
+import FirebaseDatabase
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
@@ -18,6 +18,58 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     return true
   }
 }
+
+
+struct ParkingSpot: Identifiable {
+    let id: String
+    let name: String
+    let availableSpots: Int
+}
+
+class ParkingSpotViewModel: ObservableObject {
+    @Published var parkingSpots = [ParkingSpot]()
+    let ref = Database.database(url: "https://estacionamiento-1f782-default-rtdb.firebaseio.com/").reference()
+    let parkingSpotsRef: DatabaseReference
+    
+    init() {
+        parkingSpotsRef = ref.child("parkingSpots")
+        
+        // Observar cambios en los datos de la referencia de Firebase
+        parkingSpotsRef.observe(.value) { snapshot in
+            var newParkingSpots = [ParkingSpot]()
+            for child in snapshot.children {
+                if let snapshot = child as? DataSnapshot,
+                   let value = snapshot.value as? [String: Any],
+                   let name = value["name"] as? String,
+                   let availableSpots = value["availableSpots"] as? Int {
+                    let parkingSpot = ParkingSpot(id: snapshot.key, name: name, availableSpots: availableSpots)
+                    newParkingSpots.append(parkingSpot)
+                }
+            }
+            self.parkingSpots = newParkingSpots
+        }
+    }
+}
+
+struct ParkingSpotListView: View {
+    @ObservedObject var parkingSpotViewModel = ParkingSpotViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List(parkingSpotViewModel.parkingSpots) { parkingSpot in
+                VStack(alignment: .leading) {
+                    Text(parkingSpot.name)
+                    Text("\(parkingSpot.availableSpots) spots available")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .navigationTitle("Parking Spots")
+        }
+    }
+}
+
+// ...
+
 
 
 
